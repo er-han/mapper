d5142e08f763081e33210f7372aff12fdc7e1342
Erhan Karakaya <erhan277@gmail.com>
2017-03-13 17:33:14 +0300

    Added test method for MapperUtil.getFields method

--- src/test/java/mapper/MapperUtilTest.java
+++ src/test/java/mapper/MapperUtilTest.java
@@ -6,8 +6,10 @@ import org.junit.Test;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
@@ -61,6 +63,34 @@ public class MapperUtilTest {
     fields.forEach(field -> actualFieldNames.put(field.getName(), field.getType()));
 
     assertEquals(expectedFieldNames, actualFieldNames);
+  }
+
+
+  @Test
+  public void getFieldsMap_whenHasSuperClass_returnsCorrectMap() throws IllegalAccessException {
+    final String superField1Value = "string";
+    final Integer superField2Value = 1;
+    final Double childField1Value = 3.0;
+    final List<Integer> childField2Value = new ArrayList<>();
+    childField2Value.add(30);
+
+    Map<String, Object> expectedMap = new HashMap<>();
+    expectedMap.put("superField1", superField1Value);
+    expectedMap.put("superField2", superField2Value);
+    expectedMap.put("childField1", childField1Value);
+    expectedMap.put("childField2", childField2Value);
+
+
+    TestChildClass testChildObj = new TestChildClass();
+    testChildObj.setSuperField1(superField1Value);
+    testChildObj.setSuperField2(superField2Value);
+    testChildObj.setChildField1(childField1Value);
+    testChildObj.setChildField2(childField2Value);
+
+    Map<String, Object> actualMap = MapperUtil.getFieldsMap(testChildObj);
+
+    assertEquals(expectedMap, actualMap);
+
   }
 
 
