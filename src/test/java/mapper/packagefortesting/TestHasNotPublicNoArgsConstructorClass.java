f3d950031a624d0bed811551f0e9ddcea8c8a953
Erhan Karakaya <erhan277@gmail.com>
2017-03-14 11:45:03 +0300

    Added a new class under packagefortesting package to use in ResultTypeInstantiationException test method

---
+++ src/test/java/mapper/packagefortesting/TestHasNotPublicNoArgsConstructorClass.java
@@ -0,0 +1,14 @@
+package mapper.packagefortesting;
+
+import mapper.Mappable;
+
+/**
+ * Created by erhan.karakaya on 3/14/2017.
+ */
+public class TestHasNotPublicNoArgsConstructorClass implements Mappable{
+
+
+  public TestHasNotPublicNoArgsConstructorClass(String arg) {
+  }
+
+}
