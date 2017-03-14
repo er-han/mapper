4ef69a1ea51520caa16877fc36005d72764627e8
Erhan Karakaya <erhan277@gmail.com>
2017-03-14 16:57:41 +0300

    Added a test class to be used in test methods

--- src/test/java/mapper/packagefortesting/TestHasNoSuperClass.java
+++ src/test/java/mapper/packagefortesting/TestHasNoSuperClass.java
@@ -10,6 +10,15 @@ import java.util.List;
 public class TestHasNoSuperClass implements Mappable {
   private Double childField1;
   private List<Integer> childField2;
+  private Integer superField1;
+
+  public Integer getSuperField1() {
+    return superField1;
+  }
+
+  public void setSuperField1(Integer superField1) {
+    this.superField1 = superField1;
+  }
 
   public Double getChildField1() {
     return childField1;
@@ -26,4 +35,5 @@ public class TestHasNoSuperClass implements Mappable {
   public void setChildField2(List<Integer> childField2) {
     this.childField2 = childField2;
   }
+
 }
