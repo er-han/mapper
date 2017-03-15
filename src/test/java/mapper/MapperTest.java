5ada094acb1fa0b9c921b54f5a73cae052b1d1e4
Erhan Karakaya <erhan277@gmail.com>
2017-03-15 10:31:06 +0300

    Added MapperTest class

---
+++ src/test/java/mapper/MapperTest.java
@@ -0,0 +1,48 @@
+package mapper;
+
+import mapper.exception.ResultTypeInstantiationException;
+import mapper.packagefortesting.TestChildClass;
+import mapper.packagefortesting.TestHasNoSuperClass;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by erhan.karakaya on 3/15/2017.
+ */
+public class MapperTest {
+
+  private TestChildClass mapFromObj;
+  private TestHasNoSuperClass mapToObj;
+
+  private static final String superField1Value = "STR";
+  private static final Integer superField2Value = 99;
+  private static final Double childField1Value = 120.3;
+  private static final List<Integer> childField2Value = new ArrayList<>(Arrays.asList(6, 8, 9, 445));
+
+  @Before
+  public void setUp() {
+    mapFromObj = new TestChildClass();
+    mapFromObj.setSuperField1(superField1Value);
+    mapFromObj.setSuperField2(superField2Value);
+    mapFromObj.setChildField1(childField1Value);
+    mapFromObj.setChildField2(childField2Value);
+  }
+
+  @Test
+  public void shouldReturnMapperFromInstantce_whenGetMapperIsInvoked() {
+    try {
+      mapToObj = Mapper.getMapperFrom(mapFromObj).mapTo(TestHasNoSuperClass.class);
+
+      assertNotNull(mapFromObj);
+    } catch (ResultTypeInstantiationException e) {
+      fail();
+      e.printStackTrace();
+    }
+  }
+}
