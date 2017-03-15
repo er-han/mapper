f931030009b9d10aeef6f53bd36313723f7edd6d
Erhan Karakaya <erhan277@gmail.com>
2017-03-15 10:30:44 +0300

    Fixed Mapper
    Added implementetion from MapTo.
    
    Made the constructor private
    
    Added a static builder method

--- src/main/java/mapper/Mapper.java
+++ src/main/java/mapper/Mapper.java
@@ -1,7 +1,25 @@
 package mapper;
 
+import mapper.exception.ResultTypeInstantiationException;
+import mapper.util.MapperUtil;
+
 /**
  * Created by erhan.karakaya on 3/10/2017.
  */
-public class Mapper {
+public class Mapper implements MapTo {
+  private Mappable source;
+  private Mapper(Mappable source) {
+    this.source = source;
+  }
+
+
+  public static MapTo getMapperFrom(Mappable source) {
+    return new Mapper(source);
+  }
+
+
+  @Override
+  public <T extends Mappable> T mapTo(Class<T> resultType) throws ResultTypeInstantiationException {
+    return MapperUtil.map(source, resultType);
+  }
 }
