57baa3d5c5186f1cb0cf26af5317d340630fad6d
Erhan Karakaya <erhan277@gmail.com>
2017-03-15 17:09:40 +0300

    Fixed checkstyle errors

--- src/main/java/mapper/exception/ResultTypeInstantiationException.java
+++ src/main/java/mapper/exception/ResultTypeInstantiationException.java
@@ -5,16 +5,29 @@ package mapper.exception;
  */
 public class ResultTypeInstantiationException extends Exception {
 
-  private String resultTypeInstantiationExceptionMessage = "Can not instantiate a new object of given result type: %s. The result type must have a public no-args constructor.";
+  private String resultTypeInstantiationExceptionMessage = "Can not "
+      + "instantiate a new object of given result type: "
+      + "%s. The result type must have a public no-args constructor.";
 
 
+  /**
+   * Constructor, used when result type is known.
+   * @param resultType The type which couldn't be instantiated.
+   */
   public ResultTypeInstantiationException(Class<?> resultType) {
-    String message = String.format(resultTypeInstantiationExceptionMessage, resultType.getTypeName());
+    String message = String.format(resultTypeInstantiationExceptionMessage,
+        resultType.getTypeName());
     new ResultTypeInstantiationException(message);
   }
 
+  /**
+   * Constructor, used when result type is known.
+   * @param resultType The type which couldn't be instantiated.
+   * @param cause Throwable object, if any, which has caused the exception.
+   */
   public ResultTypeInstantiationException(Class<?> resultType, Throwable cause) {
-    String message = String.format(resultTypeInstantiationExceptionMessage, resultType.getTypeName());
+    String message = String.format(resultTypeInstantiationExceptionMessage,
+        resultType.getTypeName());
     new ResultTypeInstantiationException(message, cause);
   }
 
