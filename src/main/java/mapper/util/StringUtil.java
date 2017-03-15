329ba9dd1b2a72d81678fb2ab4dcf4a6a2025ea9
Erhan Karakaya <erhan277@gmail.com>
2017-03-15 17:22:41 +0300

    Fixed checkstyle errors

--- src/main/java/mapper/util/StringUtil.java
+++ src/main/java/mapper/util/StringUtil.java
@@ -6,17 +6,18 @@ package mapper.util;
 public class StringUtil {
   /**
    * Makes the first letter uppercase of the given String.
+   *
    * @param str will be used in capitaling operation.
    * @return capitalized state of the given param.
    */
   public static String capitalizeFirstLetter(String str) {
-    if (str == null  || str.length() == 0) {
+    if (str == null || str.length() == 0) {
       return str;
     }
     StringBuilder stringBuilder = new StringBuilder();
 
     stringBuilder.append(Character.toTitleCase(str.charAt(0)));
-    if(str.length()>1) {
+    if (str.length() > 1) {
       stringBuilder.append(str.substring(1));
     }
     return stringBuilder.toString();
